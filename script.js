23859eba186a76c7600ed194e8d22f15bfb5a938
Roy Carter <roycarter@Roys-MacBook-Pro.local>
2023-01-29 20:43:32 -0500

    first attempt to paint, only works on the very last grid, loop might not be the way

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-for (var i = 0; i < 5; i++) {
+// let user set number of squares in grid
+
+let val = prompt("Pick a Size, Number between 1 and 100")
+
+//sets up display of grid on user size
+
+for (var i = 0; i < val; i++) {
 	var coll = document.createElement("div")
 	coll.className = "coll"
-	for (var j = 0; j < 5; j++) {
+	for (var j = 0; j < val; j++) {
 		var row = document.createElement("div")
 		row.className = "row"
 		coll.appendChild(row)
 	}
 	document.getElementById("container").appendChild(coll)
+}
+
+//first attempt to paint grid squares - only works on last grid - no go
+var box = document.getElementsByClassName("row")
+var test = box.length
+
+for (let i = 0; i < test; i++) {
+	box[i].addEventListener("mouseover", (event) => {
+		row.className = "yellow"
+	})
 }
