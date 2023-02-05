8ccec8aee55f12be9bb916415ee04f7b75ed75f9
Roy Carter <roycartercpa@gmail.com>
2023-02-04 21:28:15 -0500

    final on etch a sketch project, I like the clean look

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // let user set number of squares in grid
-
-let val = prompt("Pick a Size, Number between 1 and 100")
+let val = prompt("Enter your brush size between 2 and 150")
+if (val < 2 || val > 150) {
+	alert("Please Choose A Value Between 2 and 150")
+	reload()
+}
+//sets fixed grid size and variable pixels based on number
+let size = 600 / val + "px"
+console.log(size)
+let brushColor = "gray"
 
 //sets up display of grid on user size
 
@@ -11,16 +18,26 @@ for (var i = 0; i < val; i++) {
 		var row = document.createElement("div")
 		row.className = "row"
 		coll.appendChild(row)
+		row.style.width = size
+		row.style.height = size
 	}
 	document.getElementById("container").appendChild(coll)
 }
 
-//first attempt to paint grid squares - only works on last grid - no go
+//sets listener on mouse to paint
 var box = document.getElementsByClassName("row")
 var test = box.length
 
-for (let i = 0; i < test; i++) {
-	box[i].addEventListener("mouseover", (event) => {
-		row.className = "yellow"
+for (var i = 0; i < test; i++) {
+	box[i].addEventListener("mouseover", function (e) {
+		e.target.style.background = brushColor
 	})
+}
+//links to buttons to change color
+function change(color) {
+	brushColor = color
+}
+//links to new sketch button to reload page
+function reload() {
+	location.reload()
 }
